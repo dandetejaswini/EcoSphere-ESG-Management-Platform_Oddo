86e9d7516cfc26fd50b4d4c638ef278f3c75f7a6
Dande Tejaswini <dandetejaswini1329@gmail.com>
2026-07-12 15:24:16 +0530

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,12 +1,13 @@
 {
-    'name': 'EcoSphere: ESG Management Platform',
+    'name': 'EcoSphere ESG',
     'version': '1.0',
-    'category': 'Management',
-    'summary': 'Full ESG Tracking and Gamification Platform',
-    'depends': ['base', 'hr', 'product'],
-    'data': [
+    'category': 'Sustainability',
+    'summary': 'Environmental, Social, and Governance Tracking',
+    'depends': ['base', 'hr'],
+   'data': [
         'security/ir.model.access.csv',
         'views/esg_views.xml',
+        'data/esg_demo_data.xml',
     ],
     'installable': True,
     'application': True,
